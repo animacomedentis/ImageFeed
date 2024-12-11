219fc3b1ed07f0755fac0d8a3e75ef6303d808a7
Максим Карпов <maximmangk5@gmail.com>
2024-12-11 17:04:22 +0300

    no message

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -1,12 +1,14 @@
 import UIKit
+import ProgressHUD
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        return true
-    }
+        ProgressHUD.animationType = .activityIndicator
+        ProgressHUD.colorHUD = .black
+        ProgressHUD.colorAnimation = .lightGray
+        return true    }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
